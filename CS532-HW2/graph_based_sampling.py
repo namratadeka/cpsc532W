56f8b295f4d6ce9cf09c1cf5ba9c759e18e4c272
Namrata Deka <namratadeka5@gmail.com>
2021-02-16 19:38:16 -0800

    graph-based sampling complete

--- CS532-HW2/graph_based_sampling.py
+++ CS532-HW2/graph_based_sampling.py
@@ -9,6 +9,11 @@ from tests import is_tol, run_prob_test,load_truth
 from ops import env
 from evaluation_based_sampling import evaluate
 
+from tqdm import tqdm
+import numpy as np 
+import matplotlib.pyplot as plt 
+from seaborn import heatmap
+
 
 def deterministic_eval(exp):
     "Evaluation function for the deterministic target language of the graph based representation."
@@ -105,7 +110,8 @@ if __name__ == '__main__':
     for i in range(1,5):
         graph = daphne(['graph','-i','../CS532-HW2/programs/{}.daphne'.format(i)])
         print('\n\n\nSample of prior of program {}:'.format(i))
-        ret = sample_from_joint(graph)
-        print(ret)    
-
-    
+        samples = list()
+        for j in tqdm(range(1000)):
+            samples.append(sample_from_joint(graph))
+        
+        print(samples)
